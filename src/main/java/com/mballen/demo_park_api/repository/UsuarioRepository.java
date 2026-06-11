35a05d6ea6f81883434a3015bf2b40ec9b45cbe4
Pedro <pedrolucascabral4@gmail.com>
2026-06-11 15:39:39 -0300

    feat: initialize project structure with Spring Boot, Maven wrapper, and base user management components

---
+++ src/main/java/com/mballen/demo_park_api/repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package com.mballen.demo_park_api.repository;
+
+import com.mballen.demo_park_api.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
---
+++ src/main/java/com/mballen/demo_park_api/web/controller/UsuarioController.java
@@ -0,0 +1,28 @@
+package com.mballen.demo_park_api.web.controller;
+
+import com.mballen.demo_park_api.entity.Usuario;
+import com.mballen.demo_park_api.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/v1/usuarios")
+public class UsuarioController {
+
+    public final UsuarioService usuarioService;
+
+    //Mapeamento do spring
+    @PostMapping
+    //Usuario como argumento pra ter os mesmos campos do cliente
+    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario) {
+       Usuario user = usuarioService.salvar(usuario);
+       return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+}
